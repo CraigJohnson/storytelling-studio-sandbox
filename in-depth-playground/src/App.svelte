a65fd564c8707719421f4a5bed289906d4ca1ac0
Craig Johnson <crjohnso@gannett.com>
2020-05-29 09:44:13 -0500

    Adds in-depth-playground svelte app

---
+++ in-depth-playground/src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+import InDepthNav from "./components/InDepthNav.svelte";
+import HelloWorld from "./components/HelloWorld.svelte";
+import ComponentTwo from "./components/ComponentTwo.svelte";
+
+let activeComponent = HelloWorld;
+let links = [
+  { label: "Hello World", component: HelloWorld },
+  { label: "Component Two", component: ComponentTwo },
+];
+
+</script>
+
+<style>
+.in-depth-content {
+  padding-top: 20px;
+}
+</style>
+
+<div class="article-inner">
+  <InDepthNav {links} on:select="{ ({detail:{component}}) => activeComponent = component }"></InDepthNav>
+  <section class="in-depth-content">
+    <svelte:component this={activeComponent}></svelte:component>
+  </section>
+</div>
---
+++ in-depth-playground/src/components/ComponentTwo.svelte
@@ -0,0 +1,4 @@
+<div class="center-well">
+  <h1>Component Two</h1>
+  <p>Corpore aspernari. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+</div>
